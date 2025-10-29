af6657d5e68de3e79365ced3124ba3a3577a2afc
pgy103 <a88235140@gmail.com>
2025-10-29 20:01:21 +0900

    chap7

--- chap7/rdlock.c
+++ chap7/rdlock.c
@@ -1,52 +1,66 @@
 #include <stdio.h>
-#include <sys/types.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
-#include <stdlib.h>
-#include "student.h"
+#include <string.h>
 
-/* 잠금을 이용한 학생 데이터베이스 질의 프로그램 */
-#define START_ID 1001001  // 학번 시작 번호 (test.txt 첫 학생 ID 기준)
+struct student {
+    int id;
+    char name[20];
+    int score;
+};
 
 int main(int argc, char *argv[]) {
-    int fd, id;
-    struct student record;
+    int fd;
     struct flock lock;
+    struct student s;
+    int id, found = 0;
+    FILE *fp;
+    char line[100];
 
     if (argc < 2) {
-        fprintf(stderr, "How to use: %s File\n", argv[0]);
+        fprintf(stderr, "Usage: %s file\n", argv[0]);
         exit(1);
     }
 
-    if ((fd = open(argv[1], O_RDONLY)) == -1) {
+    fd = open(argv[1], O_RDONLY);
+    if (fd == -1) {
         perror(argv[1]);
         exit(2);
     }
 
-    printf("\nEnter StudentID to search : ");
-    while (scanf("%d", &id) == 1) {
-        lock.l_type = F_RDLCK;
-        lock.l_whence = SEEK_SET;
-        lock.l_start = (id - START_ID) * sizeof(record);
-        lock.l_len = sizeof(record);
+    printf("Enter StudentID to search: ");
+    scanf("%d", &id);
 
-        if (fcntl(fd, F_SETLKW, &lock) == -1) { // 읽기 잠금
-            perror(argv[1]);
-            exit(3);
+    // 읽기 잠금 설정
+    lock.l_type = F_RDLCK;
+    lock.l_whence = SEEK_SET;
+    lock.l_start = 0;
+    lock.l_len = 0;
+    fcntl(fd, F_SETLKW, &lock);
+
+    fp = fdopen(fd, "r");
+
+    // 한 줄씩 읽으며 검색
+    while (fgets(line, sizeof(line), fp)) {
+        sscanf(line, "%d %s %d", &s.id, s.name, &s.score);
+        if (s.id == id) {
+            printf("Name:%s\tStuID:%d\tScore:%d\n", s.name, s.id, s.score);
+            found = 1;
+            break;
         }
-
-        lseek(fd, (id - START_ID) * sizeof(record), SEEK_SET);
-        if ((read(fd, (char *)&record, sizeof(record)) > 0) && (record.id != 0))
-            printf("Name: %s\t StuID: %d\t Score: %d\n", record.name, record.id, record.score);
-        else
-            printf("No record %d\n", id);
-
-        lock.l_type = F_UNLCK;
-        fcntl(fd, F_SETLK, &lock); // 잠금 해제
-        printf("\nEnter StudentID to search : ");
     }
 
+    if (!found)
+        printf("Student ID %d not found.\n", id);
+
+    fclose(fp);
+
+    // 잠금 해제
+    lock.l_type = F_UNLCK;
+    fcntl(fd, F_SETLK, &lock);
     close(fd);
-    exit(0);
+
+    return 0;
 }
 
--- chap7/wrlock.c
+++ chap7/wrlock.c
@@ -1,61 +1,84 @@
 #include <stdio.h>
-#include <sys/types.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
-#include <stdlib.h>
-#include "student.h"
+#include <string.h>
 
-/* 잠금을 이용한 학생 데이터베이스 수정 프로그램 */
-#define START_ID 1001001  // 학생 ID 시작 번호 (test.dat의 첫 학번 기준)
+struct student {
+    int id;
+    char name[20];
+    int score;
+};
 
 int main(int argc, char *argv[]) {
-    int fd, id;
-    struct student record;
+    int fd;
     struct flock lock;
+    struct student s;
+    int id, found = 0;
+    FILE *fp;
+    char line[100];
 
     if (argc < 2) {
-        fprintf(stderr, "How to use: %s File\n", argv[0]);
+        fprintf(stderr, "Usage: %s file\n", argv[0]);
         exit(1);
     }
 
-    if ((fd = open(argv[1], O_RDWR)) == -1) {
+    // 파일 열기 (읽기/쓰기 모드)
+    fd = open(argv[1], O_RDWR);
+    if (fd == -1) {
         perror(argv[1]);
         exit(2);
     }
 
-    printf("\nEnter StudentID you want to modify: ");
-    while (scanf("%d", &id) == 1) {
-        lock.l_type = F_WRLCK;  // 쓰기 잠금
-        lock.l_whence = SEEK_SET;
-        lock.l_start = (id - START_ID) * sizeof(record);
-        lock.l_len = sizeof(record);
+    printf("Enter StudentID you want to modify: ");
+    scanf("%d", &id);
 
-        if (fcntl(fd, F_SETLKW, &lock) == -1) {
-            perror(argv[1]);
-            exit(3);
-        }
+    // 쓰기 잠금 설정
+    lock.l_type = F_WRLCK;
+    lock.l_whence = SEEK_SET;
+    lock.l_start = 0;
+    lock.l_len = 0;
+    fcntl(fd, F_SETLKW, &lock);
 
-        lseek(fd, (id - START_ID) * sizeof(record), SEEK_SET);
+    // 파일 스트림 연결
+    fp = fdopen(fd, "r+");
 
-        if ((read(fd, (char *)&record, sizeof(record)) > 0) && (record.id != 0)) {
-            printf("Name: %s\t StuID: %d\t Score: %d\n",
-                   record.name, record.id, record.score);
-            printf("Enter new score: ");
-            scanf("%d", &record.score);
-
-            lseek(fd, -sizeof(record), SEEK_CUR);
-            write(fd, (char *)&record, sizeof(record));
-
-            lock.l_type = F_UNLCK;
-            fcntl(fd, F_SETLK, &lock);  // 잠금 해제
-        } else {
-            printf("No record %d\n", id);
-        }
-
-        printf("\nEnter StudentID you want to modify: ");
+    // 임시 파일 생성
+    FILE *tempfp = fopen("temp.txt", "w");
+    if (!tempfp) {
+        perror("temp.txt");
+        exit(3);
     }
 
+    // 한 줄씩 읽으면서 수정
+    while (fgets(line, sizeof(line), fp)) {
+        sscanf(line, "%d %s %d", &s.id, s.name, &s.score);
+        if (s.id == id) {
+            found = 1;
+            printf("Name:%s\tStuID:%d\tScore:%d\n", s.name, s.id, s.score);
+            printf("Enter new score: ");
+            scanf("%d", &s.score);
+        }
+        fprintf(tempfp, "%d %s %d\n", s.id, s.name, s.score);
+    }
+
+    // 파일 갱신
+    freopen(argv[1], "w", fp);
+    rewind(tempfp);
+    while (fgets(line, sizeof(line), tempfp))
+        fputs(line, fp);
+
+    fclose(tempfp);
+    fclose(fp);
+
+    // 잠금 해제
+    lock.l_type = F_UNLCK;
+    fcntl(fd, F_SETLK, &lock);
     close(fd);
-    exit(0);
+
+    if (!found)
+        printf("Student ID %d not found.\n", id);
+
+    return 0;
 }
 
